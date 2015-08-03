6bad5626c1bafd8cddd4ea37d3d6df9f861cacc2
NDGO <durnov.maxim@gmail.com>
2015-08-03 12:36:00 +0300

    Added: service js events

--- js/script.js
+++ js/script.js
@@ -77,3 +77,26 @@ window.addEventListener("keydown", function (event) {
         }
     }
 });
+
+var serviceLinks = document.querySelectorAll(".service-list-column a");
+var serviceBlocks = document.querySelectorAll(".service-details-block");
+var j, h, k;
+
+for (i = 0; i < serviceLinks.length; ++i) {
+	serviceLinks[i].addEventListener("click", function (event) {
+		event.preventDefault(event);
+        for (j = 0; j < serviceLinks.length; ++j) {
+            serviceLinks[j].classList.remove("service-list-active");        
+        }
+        for (h = 0; h < serviceLinks.length; ++h) {
+            if (serviceLinks[h] == this) {
+                serviceLinks[h].classList.add("service-list-active");
+                for (k = 0; k < serviceBlocks.length; ++k) {
+                    serviceBlocks[k].classList.remove("service-details-active");
+                }
+                serviceBlocks[h].classList.add("service-details-active");
+            }
+        }
+	})
+}
+
